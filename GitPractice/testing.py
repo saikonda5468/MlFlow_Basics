351762afd1af40d197cd7761b8e37830af8be2f6
Saikonda <saikondakrishna@gmail.com>
2023-01-12 22:04:38 -0500

    modifeiddddd

--- GitPractice/testing.py
+++ GitPractice/testing.py
@@ -4,4 +4,10 @@ def adding(a,b){
          Print("greater than three")
         else:
          Print("Not greater than ten")
-
+def subtradef adding(a,b){
+            c = a+b;
+                    if c>10:
+                             Print("greater than three")
+                                     else:
+                                              Print("Not greater than ten")
+1053
